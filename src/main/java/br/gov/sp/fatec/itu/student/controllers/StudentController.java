c7e27c02ea05fcbb54a0138567f9b450a391209f
LucasMorais42 <lucasmorais2214@gmail.com>
2025-06-17 10:56:18 -0300

    Alterações Controllers e Services

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -4,8 +4,12 @@ package br.gov.sp.fatec.itu.student.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.gov.sp.fatec.itu.student.entities.Student;
 
 import br.gov.sp.fatec.itu.student.services.StudentService;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("students")
+@CrossOrigin
 public class StudentController {
 
     @Autowired
@@ -33,6 +39,18 @@ public class StudentController {
     public ResponseEntity<Student> save(@RequestBody Student student){
         return ResponseEntity.created(null)
                              .body(service.save(student));
+    }
+    
+    @DeleteMapping("{id}") //Os métodos são os protocolos HTTP.
+    public ResponseEntity<Void>delete(@PathVariable long id){ //O PathVariable serve como caminho igual faz o Angular, ficando students/1 por exemplo.
+        service.delete(id);
+        return ResponseEntity.noContent().build(); //Aqui retorna o 204
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Student student){
+        service.update(student, id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
+++ src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.repositories.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class StudentService {
@@ -21,5 +22,24 @@ public class StudentService {
     public Student save(Student student){
         return repository.save(student);
     }
+
+    
+    public void update(Student student, long id){
+        Student aux = repository.getReferenceById(id); //Pegando a referência de um estudante. Quando é necessário mostrar os dados desse aluno é ideal usar o findbyId
+        aux.setCourse(student.getCourse());
+        aux.setName(student.getName());
+
+        repository.save(aux); //Esse comando na prática representa um UPDATE do SQL. Estamos salvando ele no banco de dados novamente.
+    }
+
+    public void delete(long id){ //Para remover você passa o ID do student que quer remover e não retorna nada.
+        if(repository.existsById(id)){ //Verificando se no repositório existe um aluno com o ID informado.
+            repository.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("Aluno não cadastrado!"); //Se o aluno não existir estamos retornando essa mensagem.
+        }
+    }
+ 
 
 }
